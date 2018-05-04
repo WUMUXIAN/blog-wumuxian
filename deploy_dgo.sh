c13067256b87a2b0b6a0e1a33136716ce695bfe5
Muxian Wu <wumuxian1988@gmail.com>
2018-05-04 18:40:08 +0800

    Updated the docker run settings, make it survive through the machine restarts.

--- deploy_dgo.sh
+++ deploy_dgo.sh
@@ -2,4 +2,4 @@
 # This deployment script deploy the image using Kubernetes
 version=`cat currentVersion`
 ansible -i "139.59.247.115," all -u root -m shell -a "docker rm -f blog"
-ansible -i "139.59.247.115," all -u root -m shell -a "docker run -d -p 80:8080 --name blog  wumuxian/blog:v${version}"
+ansible -i "139.59.247.115," all -u root -m shell -a "docker run --restart always -d -p 80:8080 --name blog  wumuxian/blog:v${version}"
