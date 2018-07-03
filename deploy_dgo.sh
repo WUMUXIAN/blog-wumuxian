f3f7563a9ad4110c0b2e9ab096bde32f82a39790
Muxian Wu <wumuxian1988@gmail.com>
2018-07-03 18:18:40 +0800

    updated CI

--- deploy_dgo.sh
+++ deploy_dgo.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # This deployment script deploy the image using Kubernetes
-version=`cat currentVersion`
+# get the current version.
+current_version=$(git tag -l "releases/*" --sort=-v:refname | head -n 1)
+current_version=${current_version#"releases/v"}
+
 ansible -i "188.166.184.10," all -u root -m shell -a "docker rm -f blog"
-ansible -i "188.166.184.10," all -u root -m shell -a "docker run --restart always -d -p 8080 --name blog -l traefik.frontend.rule=Host:www.wumuxian1988.com,blog.wumuxian1988.com --network=traefik_default wumuxian/blog:v${version}"
+ansible -i "188.166.184.10," all -u root -m shell -a "docker run --restart always -d -p 8080 --name blog -l traefik.frontend.rule=Host:www.wumuxian1988.com,blog.wumuxian1988.com --network=traefik_default wumuxian/blog:v${current_version}"
