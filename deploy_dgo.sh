9d65478ae3404726822c199a41f86826ab2befc6
Muxian Wu <muxianw@twitter.com>
2021-03-04 21:22:16 +0800

    traefik v2

--- deploy_dgo.sh
+++ deploy_dgo.sh
@@ -4,5 +4,13 @@
 current_version=$(git tag -l "releases/*" --sort=-v:refname | head -n 1)
 current_version=${current_version#"releases/v"}
 
-ansible -i "188.166.184.10," all -u root -m shell -a "docker rm -f blog"
-ansible -i "188.166.184.10," all -u root -m shell -a "docker run --restart always -d -p 8080 --name blog -l traefik.frontend.rule=Host:www.wumuxian1988.com,blog.wumuxian1988.com --network=traefik_default wumuxian/blog:v${current_version}"
+command="docker run --restart always -d -p 8080 \
+	-l 'traefik.enable=true' \
+	-l 'traefik.http.routers.route0.rule=Host:(\\\`blog.wumuxian1988.com\\\`)' \
+	-l 'traefik.http.routers.route1.rule=Host:(\\\`www.wumuxian1988.com\\\`)' \
+	--name blog wumuxian/blog:v${current_version}"
+
+echo $command
+
+ansible -i "165.232.173.38," all -u root -m shell -a "docker rm -f blog"
+ansible -i "165.232.173.38," all -u root -m shell -a "$command"
